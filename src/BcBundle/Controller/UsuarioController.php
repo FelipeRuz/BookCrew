bbc4464e01fabeff537285be399a5aa1c48079a2
Felip <Felip@192.168.1.232>
2018-05-05 15:28:23 +0200

    Creación de usuario creada. Falta el cifrado de contraseña y la aparición de mensajes flash data

--- src/BcBundle/Controller/UsuarioController.php
+++ src/BcBundle/Controller/UsuarioController.php
@@ -1,9 +1,10 @@
 <?php
-
 namespace BcBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use BcBundle\Entity\Usuario;
+use BcBundle\Form\UsuarioType;
 
 class UsuarioController extends Controller
 {
@@ -17,10 +18,74 @@ class UsuarioController extends Controller
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
         
+        /*Esta parte la añadimos hasta poder usar la zona de adicion de usuario independiente de login*/
+        $usuario = new Usuario();
+        $form = $this->createForm(UsuarioType::class,$usuario);
+        
+        $form->handleRequest($request);
+        
+        If($form->isValid()){
+            $status = "Se ha registrado correctamente";
+            
+            $usuario = new Usuario();
+            $usuario->setNick($form->get("nick")->getData());
+            $usuario->setPassword($form->get("pass")->getData());
+            $usuario->setRol("USER");  
+            $usuario->setNombre($form->get("nombre")->getData());
+            $usuario->setApellido($form->get("apellido")->getData());
+            $usuario->setEmail($form->get("email")->getData());        
+            $usuario->setTlf($form->get("tlf")->getData());
+            $usuario->setPoblacion($form->get("poblacion")->getData());
+            $usuario->setProvincia($form->get("provincia")->getData());
+            $usuario->setDireccion($form->get("direccion")->getData());
+            
+            $em = $this->getDoctrine()->getEntityManager();
+            $em->persist($usuario);
+            $flush=$em->flush();
+            
+            If($flush == null ){
+                $status="El usuario se ha registrado correctamente";
+            }
+            Else{
+                $status = "NO se ha registrado correctamente";
+            }
+        }
+        Else{
+            $status = "NO se ha registrado correctamente";
+        }
+        
         return $this->render("BcBundle:Usuario:login.html.twig",array(
+            "error" => $error,
+            "last_username"=>$lastUsername,
+            "form" => $form->createView()
+        ));
+        /*return $this->render("BcBundle:Usuario:login.html.twig",array(
            "error" => $error,
            "last_username" => $lastUsername
-        ));
+        ));*/
+    }
+    
+    public function AddAction(Request $request){
+        $authenticationUtils = $this->get("security.authentication_utils");
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
         
+        $usuario = new Usuario();
+        $form = $this->createForm(UsuarioType::class,$usuario);
+        
+        $form->handleRequest($request);
+        
+        If($form->isValid()){
+            $status = "Se ha registrado correctamente";
+        }
+        Else{
+            $status = "NO se ha registrado correctamente";
+        }
+        
+        return $this->render("BcBundle:Usuario:login.html.twig",array(
+            "error" => $error,
+            "last_username"=>$lastUsername,
+            "form" => $form->createView()
+        ));
     }
 }
--- src/BcBundle/Form/UsuarioType.php
+++ src/BcBundle/Form/UsuarioType.php
@@ -6,6 +6,11 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 class UsuarioType extends AbstractType
 {
     /**
@@ -13,7 +18,37 @@ class UsuarioType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('nick')->add('pass')->add('rol')->add('nombre')->add('apellido')->add('email')->add('tlf')->add('poblacion')->add('provincia')->add('direccion');
+        $builder
+                ->add('nick',TextType::class, array("label"=>"Nick de usuario","required"=>"required","attr"=>array(
+                    "class"=> "form-name form-control",
+                )))
+                ->add('pass',PasswordType::class, array("label"=>"Contraseña","required"=>"required","attr"=>array(
+                    "class"=> "form-password form-control",
+                )))
+                ->add('nombre',TextType::class, array("label"=>"Nombre","required"=>"required","attr"=>array(
+                    "class"=> "form-name form-control",
+                )))
+                ->add('apellido',TextType::class, array("label"=>"Apellidos","required"=>"required","attr"=>array(
+                    "class"=> "form-name form-control",
+                )))
+                ->add('email',EmailType::class, array("label"=>"Email","required"=>"required","attr"=>array(
+                    "class"=> "form-name form-control",
+                )))
+                ->add('tlf',TextType::class, array("label"=>"Teléfono","required"=>"required","attr"=>array(
+                    "class"=> "form-name form-control",
+                )))
+                ->add('poblacion',TextType::class, array("label"=>"Población","required"=>"required","attr"=>array(
+                    "class"=> "form-name form-control",
+                )))
+                ->add('provincia',TextType::class, array("label"=>"Provincia","required"=>"required","attr"=>array(
+                    "class"=> "form-name form-control",
+                )))
+                ->add('direccion',TextType::class, array("label"=>"Dirección","required"=>"required","attr"=>array(
+                    "class"=> "form-name form-control",
+                )))
+                ->add('Guardar',SubmitType::class,array("attr"=>array(
+                    "class"=> "form-submit btn btn-success"
+                )));
     }/**
      * {@inheritdoc}
      */
